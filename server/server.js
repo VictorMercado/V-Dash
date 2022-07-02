92dcb6fcf96cb0ae1863f1d48916ef9a900f2c87
Victor Mercado <Victormercado@outlook.com>
2022-07-01 21:03:09 -0700

    extended server.js functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 let app = express();
-
+const path = require('path');
+const BUILD_PATH = '/Users/victormercado/LocalCode/websiteStuff/v-dash/client/build/';
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res)=> {
-    res.send("Hello my dude");
+    res.sendFile(BUILD_PATH + 'index.html');
 });
-
+app.get('/static/css/:id', (req, res)=> {
+    res.sendFile(BUILD_PATH + `/static/css/${req.params.id}`);
+});
+app.get('/static/js/:id', (req, res)=> {
+    res.sendFile(BUILD_PATH + `/static/js/${req.params.id}`);
+});
+app.get('/:id', (req, res)=> {
+    res.sendFile(BUILD_PATH + `${req.params.id}`);
+});
 app.listen(PORT, ()=> {
     console.log(`App is listening on port: ${PORT}`);
 });
